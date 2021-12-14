d591210453da1d47cddce50d7b1796bd3bc7f9fe
AllanLobato <allandonizetti@gmail.com>
2021-12-14 18:49:50 -0300

    secondCommit

--- App.js
+++ App.js
@@ -1,13 +1,195 @@
-import React, {useState, useEffect} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  TouchableOpacity,
-} from 'react-native';
-import Botao from './Botao';
+import * as React from 'react';
+import {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
+const App = () => {
+  console.disableYellowBox = true;
+
+  const [darkMode, setDarkMode] = useState(false);
+  const buttons = [
+    'AC',
+    'DEL',
+    '%',
+    '÷',
+    7,
+    8,
+    9,
+    'x',
+    4,
+    5,
+    6,
+    '-',
+    3,
+    2,
+    1,
+    '+',
+    0,
+    '.',
+    '+/-',
+    '=',
+  ];
+
+  const [currentNumber, setCurrentNumber] = useState('');
+  const [lastNumber, setLastNumber] = useState('');
+
+  function calculator() {
+    const splitNumbers = currentNumber.split(' ');
+    const fistNumber = parseFloat(splitNumbers[0]);
+    const lastNumber = parseFloat(splitNumbers[2]);
+    const operator = splitNumbers[1];
+
+    switch (operator) {
+      case '+':
+        setCurrentNumber((fistNumber + lastNumber).toString());
+        return;
+      case '-':
+        setCurrentNumber((fistNumber - lastNumber).toString());
+        return;
+      case '*':
+        setCurrentNumber((fistNumber * lastNumber).toString());
+        return;
+      case '/':
+        setCurrentNumber((fistNumber / lastNumber).toString());
+        return;
+    }
+  }
+
+  function handleInput(buttonPressed) {
+    console.log(buttonPressed);
+    if (
+      (buttonPressed === '+') |
+      (buttonPressed === '-') |
+      (buttonPressed === '*') |
+      (buttonPressed === '/')
+    ) {
+      setCurrentNumber(currentNumber + ' ' + buttonPressed + ' ');
+      return;
+    }
+    switch (buttonPressed) {
+      case 'DEL':
+        setCurrentNumber(currentNumber.substring(0, currentNumber.length - 1));
+        return;
+      case 'AC':
+        setLastNumber('');
+        setCurrentNumber('');
+        return;
+      case '=':
+        setLastNumber(currentNumber + ' = ');
+        calculator();
+        return;
+      case '+/-':
+        return;
+    }
+
+    setCurrentNumber(currentNumber + buttonPressed);
+  }
+
+  return (
+    <View>
+      <View style={styles.results}>
+        <TouchableOpacity style={styles.themeButton}>
+          <Icon
+            name={darkMode ? 'light-up' : 'moon'}
+            size={24}
+            color={darkMode ? 'white' : 'black'}
+            onPress={() => (darkMode ? setDarkMode(false) : setDarkMode(true))}
+          />
+        </TouchableOpacity>
+        <Text style={styles.historyText}>{lastNumber}</Text>
+        <Text style={styles.resultText}>{currentNumber}</Text>
+      </View>
+      <View style={styles.buttons}>
+        {buttons.map(button =>
+          button === '=' ? (
+            <TouchableOpacity
+              onPress={() => handleInput(button)}
+              key={button}
+              style={[styles.button, {backgroundColor: '#9DBC7B'}]}>
+              <Text style={[styles.textButton, {color: 'white', fontSize: 30}]}>
+                {button}
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={() => handleInput(button)}
+              key={button}
+              style={[
+                styles.button,
+                {
+                  backgroundColor:
+                    typeof button === 'number'
+                      ? darkMode === true
+                        ? '#303946'
+                        : '#fff'
+                      : darkMode === true
+                      ? '#414853'
+                      : '#ededed',
+                },
+              ]}>
+              <Text style={styles.textButton}>{button}</Text>
+            </TouchableOpacity>
+          ),
+        )}
+        ,
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  results: {
+    backgroundColor: darkMode ? '#282f3b' : '#f5f5f5',
+    width: '100%',
+    minHeight: 280,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  resultText: {
+    color: darkMode ? '#f5f5f5' : '#282F38',
+    margin: 10,
+    fontSize: 40,
+  },
+
+  historyText: {
+    color: darkMode ? '#B5B7BB' : '#7c7c7c',
+    fontSize: 20,
+    marginRight: 10,
+    alignSelf: 'flex-end',
+  },
+  themeButton: {
+    alignSelf: 'flex-start',
+    bottom: 120,
+    margin: 10,
+    backgroundColor: darkMode ? '#7b8084' : '#e5e5e5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    borderColor: darkMode ? '#3f4d5b' : '#e5e5e5',
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: 90,
+    minHeight: 90,
+    flex: 2,
+  },
+  textButton: {
+    color: darkMode ? '#b5b7bb' : '#7c7c7c',
+    fontSize: 20,
+  },
+});
+
+export default App;
+
+/*
 const App = () => {
   console.disableYellowBox = true;
 
@@ -44,7 +226,7 @@ const App = () => {
   for (var i = 0; i <= 9; i++) {
     numeros.push(i);
   }*/
-
+/*
   function clearAll() {
     setStringCalculo('0');
     setSinal(0);
@@ -58,7 +240,7 @@ const App = () => {
       setStringCalculo(parseInt(firstNumber.toString() + n.toString()));
     }
 
-    if ((n == '/' || n == '*' || n == '+' || n == '-') && secondNumber == 0) {
+    if ((n == '/' || n == 'X' || n == '+' || n == '-') && secondNumber == 0) {
       setStringCalculo(firstNumber.toString() + n);
       setSinal(n);
     }
@@ -90,13 +272,13 @@ const App = () => {
   }
 
   return (
-    <View style={{flex: 1, backgroundColor: '#121212'}}>
+    <View style={{flex: 1, backgroundColor: '#121212', alignItems: 'flex-end'}}>
       <Text style={{fontSize: 24, color: '#fff'}}>{stringCalculo}</Text>
-
+      <Icon name="light-up" size={24} color="white" />
       <View
         style={{
           display: 'flex',
-          height: '30%',
+          height: '36%',
           alignItems: 'flex-end',
         }}></View>
 
@@ -131,4 +313,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default App;*/
